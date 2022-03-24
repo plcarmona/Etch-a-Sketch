5cda67420f044e911782ef4b55350ed4f604847d
pit <plcarmona@uc.cl>
2022-03-24 17:38:49 -0300

    color on click

--- javascript.js
+++ javascript.js
@@ -1,34 +1,53 @@
 const container= document.querySelector('#container');
 container.className+= " d-flex flex-row justify-content-center"
-function rowel(fila, columna){
- for(i=0;i<fila;i++){
 
-const col = document.createElement('col');
+function rowel(filaa, columna){
+ for(i=0;i<filaa;i++){
 
-col.classList.add=('col');
+    const col = document.createElement('col');
 
-col.className = "card-group display-1 d-flex flex-column hover-overlay";
-container.appendChild(col);
-for(j=0;j<columna;j++){
-    const fila = document.createElement('div')
-    fila.classList.add=('fil');
-    fila.id='c'+i+j;
-    fila.className = "bg-white ";
-    fila.style="height: 3rem; width: 3rem;"
-    const img = document.createElement('div')
-    img.className="mask";
-    img.style= style="background: linear-gradient(45deg, rgba(29, 236, 197, 0.5),rgba(91, 14, 214, 0.5) 100%);";
-    fila.addEventListener('mouseenter',
-    ()=> fila.className="bg-primary  "
-    );
-    fila.addEventListener('',
-    ()=> fila.className="bg-white"
-    );
-    col.appendChild(fila);
-    fila.appendChild(img);
-}
+    col.classList.add=('div');
 
+    col.className = "card-group display-1 d-flex flex-column hover-overlay";
+    container.appendChild(col);
+    for(j=0;j<columna;j++){
+        const fila = document.createElement('div')
+        fila.classList.add=('div');
+        fila.id='i'+i+'j'+j;
+        fila.style="height:"+30/filaa+"rem; width:"+30/columna+"rem;"
+        fila.style.backgroundColor='white';
+        const sel = document.getElementById('i'+i+'j'+j);
+        fila.addEventListener('mouseenter', colorear);
+       //  fila.addEventListener('mouseenter', ()=> fila.style.backgroundColor = "rgba(255,255,255)"  );
+        col.appendChild(fila);
+    }
  }
 }
 
-rowel(10,10);
+let click=false;
+
+
+document.querySelector('body').addEventListener('mousedown', 
+()=> click= true
+);
+document.querySelector('body').addEventListener('mouseup', 
+()=> click= false
+);
+
+let filas=50;
+let columna=50;
+rowel(filas,columna);
+// desde aqui sera crear la funcion para pintar con clik
+function colorear(){
+   for(i=0;i<filas;i++){
+      for(j=0;j<columna;j++){
+         if(click){
+            this.addEventListener('mouseover',
+            this.style.backgroundColor='black');
+            this.addEventListener('click',
+            this.style.backgroundColor='black');
+         }
+      }
+   }
+}
+colorear();
